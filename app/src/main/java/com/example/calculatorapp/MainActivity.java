243c9f42be4ae5e652e1ca92b6c6e28189ca2bc9
Devansh Shukla <59922056+D-Coder135@users.noreply.github.com>
2021-07-01 23:30:50 +0530

    define the objects of the Button class inside the class MainActivity.java

--- app/src/main/java/com/example/calculatorapp/MainActivity.java
+++ app/src/main/java/com/example/calculatorapp/MainActivity.java
@@ -3,10 +3,12 @@ package com.example.calculatorapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
 import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
     EditText editText1, editText2;
+    Button add, sub, mul, div;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
