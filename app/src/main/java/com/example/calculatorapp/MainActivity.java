c6b73273f116cdd6d6ee978266943f888c9b18b9
Devansh Shukla <59922056+D-Coder135@users.noreply.github.com>
2021-07-01 23:32:17 +0530

    Initialize the object editText2 with the method call of the method findViewById()

--- app/src/main/java/com/example/calculatorapp/MainActivity.java
+++ app/src/main/java/com/example/calculatorapp/MainActivity.java
@@ -17,5 +17,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         editText1 = (EditText)findViewById(R.id.editText);
+        editText2 = (EditText)findViewById(R.id.editText2);
     }
 }
