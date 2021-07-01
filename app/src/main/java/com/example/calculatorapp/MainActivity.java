516483cbefe65356b080b35de374365aeb79568a
Devansh Shukla <59922056+D-Coder135@users.noreply.github.com>
2021-07-01 23:30:08 +0530

    define the object of the EditText class inside the class MainActivity.java

--- app/src/main/java/com/example/calculatorapp/MainActivity.java
+++ app/src/main/java/com/example/calculatorapp/MainActivity.java
@@ -3,8 +3,10 @@ package com.example.calculatorapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+    EditText editText1, editText2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
